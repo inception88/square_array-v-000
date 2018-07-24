da7e019ab00a5f551a01d9244aca52f1104df129
Benjamin Jones <bljones36@gmail.com>
2018-07-24 01:39:18 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,13 @@
 def square_array(array)
+array.collect {|x| x*x }
+  end
+end
+
+def swquare_array(array)
   new_array = []
   array.each do |x|
     square = x*x
-    new_array << square
+    new_array = array.collect
   end
   new_array
 end
